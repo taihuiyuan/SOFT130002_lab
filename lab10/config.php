70cc35c36921dc5921e2dd8da083e17508034133
taihuiyuan <19302010077@fudan.com>
2020-06-07 21:30:58 +0800

    lab10

---
+++ lab10/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'test');
+define('DBPASS', '18252644853thy');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;');
+?>
